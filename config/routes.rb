19857ab3656a6769abb9f604cb7f0d1833f272f2
benhayehudi <bengreenberg@gmail.com>
2020-03-09 09:07:13 +0200

    webhook is coming in via get sometimes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  post '/webhooks/event', to: 'corona_diff#event'
+  match '/webhooks/event' => 'corona_diff#event', via: [:get, :post]
   get '/', to: 'corona_diff#index'
   post '/recipient/new', to: 'corona_diff#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
